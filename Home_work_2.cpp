9662bc34ccf9f0b57b071d1e5263950c4913e74f
Vlad-8C <vlad.gritcenko2w@mail.ru>
2025-03-13 22:32:15 +0500

    Update Home_work_2.cpp

--- Home_work_2.cpp
+++ Home_work_2.cpp
@@ -1,24 +1,21 @@
 #include <iostream>
 
-int main()
+int main_2()
 {
-    bool Arr[2]={true, false};
-    std::cout<< "|| ";
-    for (int i=0; i<2;i++)
-    {
-        for (int j=0; j<2;j++)
-        {
-            std::cout << std::boolalpha<<(Arr[i]||Arr[j])<<'\t';
-        }
+int a=1,b=-2,c=3,check;
+check=c>b
+    ?c>a
+        ?a>b?1:2
+        :3
+    :(c>a)
+        ?4
+        :(a>b)?5:6;
+switch(check){
+    case 1: std::cout<<c<<'\t'<<a<<'\t'<<b<<std::endl; break;
+    case 2: std::cout<<c<<'\t'<<b<<'\t'<<a<<std::endl; break;
+    case 3: std::cout<<a<<'\t'<<c<<'\t'<<b<<std::endl; break;
+    case 4: std::cout<<b<<'\t'<<c<<'\t'<<a<<std::endl; break;
+    case 5: std::cout<<b<<'\t'<<a<<'\t'<<c<<std::endl; break;
+    case 6: std::cout<<a<<'\t'<<b<<'\t'<<c<<std::endl; break;
     }
-    std::cout<<std::endl<<"&& ";
-    
-    for (int i=0; i<2;i++)
-    {
-        for (int j=0; j<2;j++)
-        {
-            std::cout<< std::boolalpha<<(Arr[i]&&Arr[j])<<'\t';
-        }
-    }
-    std::cout<< std::endl;
 }
